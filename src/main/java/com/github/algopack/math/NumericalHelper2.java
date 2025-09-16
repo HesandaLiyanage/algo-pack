64e6b243084632e6370476da300e28022af30e9c
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2025-09-16 19:14:30 +0530

    perf(math): add numerical algorithm optimization #2

---
+++ src/main/java/com/github/algopack/math/NumericalHelper2.java
@@ -0,0 +1,9 @@
+package com.github.algopack.math;
+
+public class NumericalHelper2 {
+    public static double compute(int n) {
+        double s = 0;
+        for (int i = 1; i <= n; i++) s += 1.0 / (i + 2);
+        return s;
+    }
+}
